167a9eca54be19e3db21d26029c53f89b1dfeb3a
Karina Williams <coleman_karina@yahoo.com>
2021-12-07 11:17:47 -0800

    updated seeds

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -2,32 +2,32 @@ const { Comment } = require('../models');
 
 const commentData = [
   {
-    comment_text: "Wow! I didn't know about partials. Thanks!",
+    comment_text: "This assignment was fun to do",
     post_id: 3,
     user_id: 1
   },
   {
-    comment_text: "TEXT is another useful data type for entering long form text data.",
+    comment_text: "pretty straightforward",
     post_id: 1,
     user_id: 4
   },
   {
-    comment_text: "Ah, so that's what cookies are good for (besides eating ~.0)!",
+    comment_text: "learning about SQL was my favorite",
     post_id: 4,
     user_id: 2
   },
   {
-    comment_text: "Great. Now I'm hungry for cookies.",
+    comment_text: "6 months went by super fast",
     post_id: 4,
     user_id: 3
   },
   {
-    comment_text: "Did you know about salting?",
+    comment_text: "I cant believe it",
     post_id: 5,
     user_id: 5
   },
   {
-    comment_text: "Yeah! Adding that random data is a great way to force a unique hashed output, even when your users all pick 'password' for their password >.< ",
+    comment_text: "one week left",
     post_id: 5,
     user_id: 4
   },
