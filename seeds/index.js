079cf6659c68c9da705cb3adc30a11d274383e4a
Karina Williams <coleman_karina@yahoo.com>
2021-12-15 10:40:27 -0800

    went back and made changes in various files

--- seeds/index.js
+++ seeds/index.js
@@ -4,18 +4,11 @@ const seedComments = require('./comment-seeds');
 
 const sequelize = require('../config/connection');
 
-const seedAll = async () => {
+const seedAll = async() => {
   await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
   await seedUsers();
-  console.log('\n----- USERS SEEDED -----\n');
-
   await seedPosts();
-  console.log('\n----- POSTS SEEDED -----\n');
-
   await seedComments();
-  console.log('\n----- COMMENTS SEEDED -----\n');
-
   process.exit(0);
 };
 
